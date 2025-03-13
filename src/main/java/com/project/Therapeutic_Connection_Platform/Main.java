73e6a8ddb66abb8e6fc682ee8e47805a8de105d4
sena1771 <enginsena00@gmail.com>
2025-03-13 23:27:04 +0300

    springboot initialization

---
+++ src/main/java/com/project/Therapeutic_Connection_Platform/Main.java
@@ -0,0 +1,13 @@
+package com.project.Therapeutic_Connection_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Therapeutic_Connection_Platform/TherapeuticConnectionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Therapeutic_Connection_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherapeuticConnectionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
