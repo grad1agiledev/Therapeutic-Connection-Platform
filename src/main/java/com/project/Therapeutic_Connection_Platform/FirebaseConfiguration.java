6fb2dcd1462a68a303d461b4b65cd441f9f05c00
sena1771 <enginsena00@gmail.com>
2025-03-14 20:20:54 +0300

    firebase configuration path hiden for the security of DB

--- src/main/java/com/project/Therapeutic_Connection_Platform/FirebaseConfiguration.java
+++ src/main/java/com/project/Therapeutic_Connection_Platform/FirebaseConfiguration.java
@@ -2,24 +2,29 @@ package com.project.Therapeutic_Connection_Platform;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfiguration {
 
-  public void init() throws IOException
-  {
-      InputStream firebaseServiceKey = this.getClass().getResourceAsStream("../resources/therapeutic-connection-firebase-adminsdk-fbsvc-4d0e0fc357.json");
-      FirebaseOptions firebaseOptions = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(firebaseServiceKey)).build();
+    @Value("${FIREBASE_CONFIG_FILE_PATH}")
+    private String firebaseConfigFilePath;
 
-      if (FirebaseApp.getApps().isEmpty())
-      {
-          FirebaseApp.initializeApp(firebaseOptions);
-      }
-  }
+    public void init() throws IOException {
 
+        try (InputStream firebaseServiceKey = new FileInputStream(firebaseConfigFilePath))
+        {
+            FirebaseOptions firebaseOptions = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(firebaseServiceKey)).build();
 
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(firebaseOptions);
+            }
+        }
+
+    }
 }
