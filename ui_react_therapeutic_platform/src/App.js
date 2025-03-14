b16c7c2dd634fea5d83d867a13113f46c43ec034
sena1771 <enginsena00@gmail.com>
2025-03-14 16:06:21 +0300

    firebase configs and integrates,patient registration completed, web react developments added

---
+++ ui_react_therapeutic_platform/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Register from './Registration/Register'
+
+function App() {
+  return (
+       <div>
+         <h1>Welcome Patient</h1>
+         <Register />
+       </div>
+     );
+   }
+
+export default App;
