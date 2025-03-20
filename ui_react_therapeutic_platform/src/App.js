03975dfb2bb71f01ab84bd7b213345107180a7e0
fatih.zengin <fatih.zengin@ozu.edu.tr>
2025-03-20 12:35:53 +0300

    Added backend logic to fetch user meetings

--- ui_react_therapeutic_platform/src/App.js
+++ ui_react_therapeutic_platform/src/App.js
@@ -7,6 +7,8 @@ function App() {
 
   const [isVideoConferenceActive, setIsVideoConferenceActive] = useState(false);
   const isDevelopment = process.env.REACT_APP_ENV === 'development';
+  //TODO: Replace this with the actual user ID of the logged in user
+  const currentUserId = "1";
   
   const handleStartVideoConference = () => {
     // Toggle video conference state
@@ -32,7 +34,7 @@ function App() {
       
       <main className="app-main">
       {isVideoConferenceActive ? (
-          <VideoConference />
+          <VideoConference userId={currentUserId} />
         ) : (
           <TherapistList />
         )}
