33fdab4177402242e13739a681ef0f747f7466a5
sena1771 <enginsena00@gmail.com>
2025-04-13 18:49:29 +0300

    login page added

---
+++ ui_react_therapeutic_platform/src/Login/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { auth } from '../firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const userInfos = await signInWithEmailAndPassword(auth, email, password);
+      console.log('User logged in:', userInfos.user);
+      alert('Login successful!');
+      navigate('/');
+    } catch (error) {
+      console.error('Login error:', error.message);
+      alert('Login failed: ' + error.message);
+    }
+  };
+
+  return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          /><br/>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          /><br/>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+
+  export default Login;
