b5ce21b5130b9031641100d531c08749936b73a2
sena1771 <enginsena00@gmail.com>
2025-04-18 13:23:52 +0300

    therapist and admin licence doc sending and approving feature added

---
+++ src/main/java/com/project/Therapeutic_Connection_Platform/dto/VerificationDecision.java
@@ -0,0 +1,9 @@
+package com.project.Therapeutic_Connection_Platform.dto;
+
+import com.project.Therapeutic_Connection_Platform.modelEnums.VerificationState;
+
+public class VerificationDecision {
+
+    public VerificationState decision;
+    public String notes;
+}
