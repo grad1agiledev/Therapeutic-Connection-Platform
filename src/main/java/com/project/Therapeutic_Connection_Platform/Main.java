396b222736e74070ad3c1e765c9c7dd8d13679d4
sena1771 <enginsena00@gmail.com>
2025-05-16 13:32:34 +0300

    firebase secret file added

--- src/main/java/com/project/Therapeutic_Connection_Platform/Main.java
+++ src/main/java/com/project/Therapeutic_Connection_Platform/Main.java
@@ -7,10 +7,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.setProperty("FIREBASE_CONFIG_FILE_PATH", "/etc/secrets/firebase-key.json");
+
 		System.setProperty("STRIPE_API_KEY", System.getenv("STRIPE_API_KEY"));
 		System.setProperty("STRIPE_WEBHOOK_SECRET", System.getenv("STRIPE_WEBHOOK_SECRET"));
-		System.setProperty("FIREBASE_CONFIG_FILE_PATH", System.getenv("FIREBASE_CONFIG_FILE_PATH"));
+		//System.setProperty("FIREBASE_CONFIG_FILE_PATH", System.getenv("FIREBASE_CONFIG_FILE_PATH"));
 		SpringApplication.run(Main.class, args);
 	}
 
